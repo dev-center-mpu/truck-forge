8a1ed01ce9b9a079c45e27f72e1dcc3408814151
Alexey <besedinalexey@gmail.com>
2019-10-01 21:36:54 +0300

    Added bootstrap, removed default page, added SetUp component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {SetUpComponent} from './set-up/set-up.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: SetUpComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
+r
