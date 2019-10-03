a48375142c0ebef35c1401c3e43693073f07fb72
Alexey <besedinalexey@gmail.com>
2019-10-04 00:09:26 +0300

    Removed redundant code

--- src/app/components/initial-set-up/initial-set-up.component.ts
+++ src/app/components/initial-set-up/initial-set-up.component.ts
@@ -1,22 +1,9 @@
-import {Component, OnInit} from '@angular/core';
-import {DatabaseService} from '../../services/database.service';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-set-up',
   templateUrl: './initial-set-up.component.html',
   styleUrls: ['./initial-set-up.component.scss']
 })
-export class InitialSetUpComponent implements OnInit {
-
-
-  private pallets: [object];
-
-  constructor(private database: DatabaseService) {
-    this.trucks = this.database.trucks;
-    this.pallets = this.database.trucks;
-  }
-
-  ngOnInit() {
-  }
-
+export class InitialSetUpComponent {
 }
