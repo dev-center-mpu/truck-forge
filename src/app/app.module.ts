4ea753bed90aa242de9ec5d796dcad37afb7b401
Alexey <besedinalexey@gmail.com>
2019-10-04 23:16:16 +0300

    CheckChosenItems component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import {ReactiveFormsModule} from '@angular/forms';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {InitialSetUpComponent} from './components/initial-set-up/initial-set-up.component';
-import {TruckSetUpComponent} from './components/truck-set-up/truck-set-up.component';
 import {ChooseTruckComponent} from './components/choose-truck/choose-truck.component';
 import {ChoosePalletComponent} from './components/choose-pallet/choose-pallet.component';
 import {ChooseCargoComponent} from './components/choose-cargo/choose-cargo.component';
+import {CheckChosenItemsComponent} from './components/check-chosen-items/check-chosen-items.component';
+import {TruckSetUpComponent} from './components/truck-set-up/truck-set-up.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import {ChooseCargoComponent} from './components/choose-cargo/choose-cargo.compo
     TruckSetUpComponent,
     ChooseTruckComponent,
     ChoosePalletComponent,
-    ChooseCargoComponent
+    ChooseCargoComponent,
+    CheckChosenItemsComponent
   ],
   imports: [
     BrowserModule,
