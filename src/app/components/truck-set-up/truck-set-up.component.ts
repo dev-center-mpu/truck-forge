7c1a3bf43cabe188cfce66277066d1cdecc3f820
Artem <zmet1999@mail.ru>
2019-10-07 15:11:55 +0300

    added custom geometry

--- src/app/components/truck-set-up/truck-set-up.component.ts
+++ src/app/components/truck-set-up/truck-set-up.component.ts
@@ -5,9 +5,13 @@ import {
   SelectionChangedEventArgs,
   DocumentChangedEvent
 } from 'ng2-adsk-forge-viewer';
+import * as LMV from '../../../assets/viewer_v7.min.js';
 import { ACCESS_TOKEN, DOCUMENT_URN } from './config';
 
 import {Component, OnInit} from '@angular/core';
+// import * as THREE from 'three';
+declare const THREE: any
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-truck-set-up',
@@ -20,8 +24,10 @@ export class TruckSetUpComponent implements OnInit {
   public thumbnailOptions: ThumbnailOptions;
   public documentId: string;
 
+  viewer;
 
   constructor() {
+    
   }
 
   ngOnInit() {
@@ -46,14 +52,12 @@ export class TruckSetUpComponent implements OnInit {
       onViewerInitialized: (args: ViewerInitializedEvent) => {
         // Load document in the viewer
         args.viewerComponent.DocumentId = DOCUMENT_URN;
+        // console.log(args.viewer.overlays);
+        this.addCustomGeom(args.viewer);
       },
     };
 
   }
-
-  // public loadDocument(args: ViewerInitializedEvent) {
-  //   args.viewerComponent.DocumentId = DOCUMENT_URN;
-  // }
 
   public documentChanged(event: DocumentChangedEvent) {
     const { document } = event;
@@ -64,5 +68,41 @@ export class TruckSetUpComponent implements OnInit {
     if (viewables && viewables.length > 0) {
       event.viewerComponent.loadDocumentNode(document, viewables[0]);
     }
+  }
+
+ 
+
+  addCustomGeom(viewer){
+    // var geometry = new THREE.BoxBufferGeometry( 20, 20, 20 );
+    // var material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+    // var sphereMesh = new THREE.Mesh(geometry, material);
+    // sphereMesh.position.set(1, 2, 3);
+
+
+    // // if (!Viewer.overlays.hasScene('custom-scene')) {
+    // //   Viewer.addScene('custom-scene');
+    // // }
+    // //viewer.overlays.addScene('custom-scene');
+    // viewer.overlays.impl.addModel(sphereMesh);
+
+    // const geometry = new THREE.BoxBufferGeometry( 200, 200, 200 );
+    // const material = new THREE.MeshBasicMaterial({ color: 0x336699 });
+    // const mesh = new THREE.Mesh(geometry, material);
+    // mesh.position.x = 0.0; mesh.position.y = 0.0; mesh.position.z = 0.0;
+    // // Add scene and mesh
+    // var scene = new THREE.Scene();
+    // viewer.overlays.addScene(scene);
+
+    // viewer.overlays.impl.scene.children.push(scene);
+    // // viewer.overlays.addMesh([mesh], scene);
+    // console.log(viewer.overlays);
+
+    const geom = new THREE.SphereGeometry(10, 8, 8);
+    const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+    const sphereMesh = new THREE.Mesh(geom, material);
+    sphereMesh.position.set(1, 2, 3);
+    viewer.overlays.impl.createOverlayScene('custom-scene');
+    viewer.overlays.impl.addOverlay('custom-scene', sphereMesh);
+    viewer.overlays.impl.invalidate(true);
   }
 }
