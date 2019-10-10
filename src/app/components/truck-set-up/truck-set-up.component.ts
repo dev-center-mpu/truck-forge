a585ef82e0751ae4046bbfeca63173f192e5f018
Artem <zmet1999@mail.ru>
2019-10-10 19:57:31 +0300

    auth try

--- src/app/components/truck-set-up/truck-set-up.component.ts
+++ src/app/components/truck-set-up/truck-set-up.component.ts
@@ -5,10 +5,10 @@ import {
   SelectionChangedEventArgs,
   DocumentChangedEvent
 } from 'ng2-adsk-forge-viewer';
-import { ACCESS_TOKEN, DOCUMENT_URN } from './config';
 
-import {Component, OnInit} from '@angular/core';
-// import * as THREE from 'three';
+import * as auth from '../../../assets/auth.js';
+
+import { Component, OnInit } from '@angular/core';
 declare const THREE: any;
 import * as projector from "three/examples/js/renderers/Projector.js"
 import { from } from 'rxjs';
@@ -23,18 +23,25 @@ export class TruckSetUpComponent implements OnInit {
   public viewerOptions3d: ViewerOptions;
   public thumbnailOptions: ThumbnailOptions;
   public documentId: string;
-  
+
+  FORGE_CLIENT_ID = "MSKuogyPaWygG9PQAMGBQK1fIoAbd3ES";
+  FORGE_CLIENT_SECRET = "6esaVpljx0GL4QL9";
+
+  ACCESS_TOKEN;
+  DOCUMENT_URN;
+
   viewer;
-  
+
   raycaster; mouse; INTERSECTED;
 
   constructor() {
+    this.ACCESS_TOKEN = auth.getToken();
     this.thumbnailOptions = {
       getAccessToken: (onGetAccessToken: (token: string, expire: number) => void) => {
         const expireTimeSeconds = 60 * 30 * 1000;
-        onGetAccessToken(ACCESS_TOKEN, expireTimeSeconds);
+        onGetAccessToken(this.ACCESS_TOKEN, expireTimeSeconds);
       },
-      documentId: DOCUMENT_URN,
+      documentId: this.DOCUMENT_URN,
       width: 400,
       height: 400,
     };
@@ -44,14 +51,14 @@ export class TruckSetUpComponent implements OnInit {
         env: 'AutodeskProduction',
         getAccessToken: (onGetAccessToken: (token: string, expire: number) => void) => {
           const expireTimeSeconds = 60 * 30;
-          onGetAccessToken(ACCESS_TOKEN, expireTimeSeconds);
+          onGetAccessToken(this.ACCESS_TOKEN, expireTimeSeconds);
         },
       },
       onViewerInitialized: (args: ViewerInitializedEvent) => {
         // Load document in the viewer
-        args.viewerComponent.DocumentId = DOCUMENT_URN;
+        args.viewerComponent.DocumentId = this.DOCUMENT_URN;
         // this.mouse = new THREE.Vector2(1, 1);
-        
+
         this.viewer = args.viewer;
         console.log(this.viewer);
         document.querySelector("#forge").addEventListener('mousemove', this.onDocumentMouseMove.bind(this), false);
@@ -84,7 +91,7 @@ export class TruckSetUpComponent implements OnInit {
     //     // Load document in the viewer
     //     args.viewerComponent.DocumentId = DOCUMENT_URN;
     //     // this.mouse = new THREE.Vector2(1, 1);
-        
+
     //     this.viewer = args.viewer;
     //     console.log(this.viewer);
     //     document.querySelector("#forge").addEventListener('mousemove', this.onDocumentMouseMove, false);
@@ -96,9 +103,9 @@ export class TruckSetUpComponent implements OnInit {
 
   public documentChanged(event: DocumentChangedEvent) {
     const { document } = event;
-  
+
     if (!document.getRoot()) return;
-  
+
     const viewables = document.getRoot().search({ type: 'geometry', role: '2d' });
     if (viewables && viewables.length > 0) {
       event.viewerComponent.loadDocumentNode(document, viewables[0]);
@@ -106,8 +113,8 @@ export class TruckSetUpComponent implements OnInit {
   }
 
 
-  addCustomGeom(viewer){
-    const geom = new THREE.BoxGeometry( 20, 1, 20 );
+  addCustomGeom(viewer) {
+    const geom = new THREE.BoxGeometry(20, 1, 20);
     const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
     const sphereMesh = new THREE.Mesh(geom, material);
     sphereMesh.position.set(100, 2, 3);
@@ -118,41 +125,39 @@ export class TruckSetUpComponent implements OnInit {
     this.viewer = viewer;
   }
 
-  onDocumentMouseMove(event){
-    this.mouse = new THREE.Vector2(( event.clientX / window.innerWidth ) * 2 - 1, - ( event.clientY / window.innerHeight ) * 2 + 1);
+  onDocumentMouseMove(event) {
+    this.mouse = new THREE.Vector2((event.clientX / window.innerWidth) * 2 - 1, - (event.clientY / window.innerHeight) * 2 + 1);
 
 
     var vector = new THREE.Vector3(this.mouse.x, this.mouse.y, 1);
     vector.unproject(this.viewer.impl.camera);
-    
+
     this.raycaster = new THREE.Raycaster(this.viewer.impl.camera.position, vector.sub(this.viewer.impl.camera.position).normalize());
 
-    var intersects = this.raycaster.intersectObjects( this.viewer.impl.overlayScenes.cScene.scene.children );
+    var intersects = this.raycaster.intersectObjects(this.viewer.impl.overlayScenes.cScene.scene.children);
     // if there is one (or more) intersections
-    if ( intersects.length > 0 )
-    {
+    if (intersects.length > 0) {
       // if the closest object intersected is not the currently stored intersection object
-      if ( intersects[ 0 ].object != this.INTERSECTED )
-      {
+      if (intersects[0].object != this.INTERSECTED) {
         // restore previous intersection object (if it exists) to its original color
-        if ( this.INTERSECTED )
-        this.INTERSECTED.material.color.setHex( this.INTERSECTED.currentHex );
+        if (this.INTERSECTED)
+          this.INTERSECTED.material.color.setHex(this.INTERSECTED.currentHex);
         // store reference to closest object as current intersection object
-        this.INTERSECTED = intersects[ 0 ].object;
+        this.INTERSECTED = intersects[0].object;
         // store color of closest object (for later restoration)
         this.INTERSECTED.currentHex = this.INTERSECTED.material.color.getHex();
         // set a new color for closest object
-        this.INTERSECTED.material.color.setHex( 0x080708 );
+        this.INTERSECTED.material.color.setHex(0x080708);
       }
     }
     else // there are no intersections
     {
       // restore previous intersection object (if it exists) to its original color
-      if ( this.INTERSECTED )
-        this.INTERSECTED.material.color.setHex( this.INTERSECTED.currentHex );
-        // remove previous intersection object reference
-        //     by setting current intersection object to "nothing"
-        this.INTERSECTED = null;
-    } 
+      if (this.INTERSECTED)
+        this.INTERSECTED.material.color.setHex(this.INTERSECTED.currentHex);
+      // remove previous intersection object reference
+      //     by setting current intersection object to "nothing"
+      this.INTERSECTED = null;
+    }
   }
 }
