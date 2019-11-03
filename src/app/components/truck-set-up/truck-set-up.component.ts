20f536a1359d38c09cae859051a57a2af5492367
Alexey <besedinalexey@gmail.com>
2019-11-03 18:12:55 +0300

    Texture loading

--- src/app/components/truck-set-up/truck-set-up.component.ts
+++ src/app/components/truck-set-up/truck-set-up.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { DocumentChangedEvent, ThumbnailOptions, ViewerInitializedEvent, ViewerOptions } from 'ng2-adsk-forge-viewer';
-import { ServerForgeConnectionService } from '../../services/server-forge-connection.service';
-import { ChosenDataService } from '../../services/chosen-data.service';
+import {Component, OnInit} from '@angular/core';
+import {DocumentChangedEvent, ThumbnailOptions, ViewerInitializedEvent, ViewerOptions} from 'ng2-adsk-forge-viewer';
+import {ServerForgeConnectionService} from '../../services/server-forge-connection.service';
+import {ChosenDataService} from '../../services/chosen-data.service';
 
 declare const THREE: any;
 
@@ -12,24 +12,22 @@ declare const THREE: any;
 })
 export class TruckSetUpComponent implements OnInit {
 
-  private viewerOptions3d: ViewerOptions;
-  private thumbnailOptions: ThumbnailOptions;
+  viewer;
 
-  public viewer;
-
-  public raycaster;
-  public mouse;
-  public INTERSECTED;
+  viewerOptions3d: ViewerOptions;
+  thumbnailOptions: ThumbnailOptions;
 
   constructor(
     private serverForgeConnection: ServerForgeConnectionService,
-    private chosenData: ChosenDataService) {
+    private chosenData: ChosenDataService
+  ) {
   }
 
   async ngOnInit() {
     const authData = await this.serverForgeConnection.getData();
     const token = authData.access_token;
     const documentUrn = this.chosenData.truck.urn;
+
     this.thumbnailOptions = {
       getAccessToken: (onGetAccessToken: (token: string, expire: number) => void) => {
         const expireTimeSeconds = 60 * 30 * 1000;
@@ -51,24 +49,22 @@ export class TruckSetUpComponent implements OnInit {
       onViewerInitialized: (args: ViewerInitializedEvent) => {
         args.viewerComponent.DocumentId = documentUrn;
         this.viewer = args.viewer;
-        // document.querySelector('#forge')
-        // .addEventListener('mousemove', this.onDocumentMouseMove.bind(this), false);
         this.viewer.addEventListener(Autodesk.Viewing.SELECTION_CHANGED_EVENT, this.onDocumentMouseClick.bind(this));
         this.addCustomGeom(args.viewer);
       },
     };
   }
 
-  public documentChanged(event: DocumentChangedEvent) {
-    const { document } = event;
+  documentChanged(event: DocumentChangedEvent) {
+    const {document} = event;
 
     if (!document.getRoot()) {
       return;
     }
 
-    const viewables = document.getRoot().search({ type: 'geometry', role: '2d' });
+    const viewables = document.getRoot().search({type: 'geometry', role: '2d'});
     if (viewables && viewables.length > 0) {
-      event.viewerComponent.loadDocumentNode(document, viewables[0]);
+      event.viewerComponent.loadDocumentNode(document, viewables[0]).then();
     }
   }
 
@@ -76,40 +72,6 @@ export class TruckSetUpComponent implements OnInit {
     viewer.overlays.impl.invalidate(true);
     this.viewer = viewer;
     this.viewer.impl.createOverlayScene('cScene');
-  }
-
-  onDocumentMouseMove(event) {
-    this.mouse = new THREE.Vector2((event.clientX / window.innerWidth) * 2 - 1, -(event.clientY / window.innerHeight) * 2 + 1);
-
-    const vector = new THREE.Vector3(this.mouse.x, this.mouse.y, 1);
-    vector.unproject(this.viewer.impl.camera);
-
-    this.raycaster = new THREE.Raycaster(this.viewer.impl.camera.position, vector.sub(this.viewer.impl.camera.position).normalize());
-
-    const intersects = this.raycaster.intersectObjects(this.viewer.impl.overlayScenes.cScene.scene.children);
-    if (intersects.length > 0) {
-      // if the closest object intersected is not the currently stored intersection object
-      if (intersects[0].object !== this.INTERSECTED) {
-        // restore previous intersection object (if it exists) to its original color
-        if (this.INTERSECTED) {
-          this.INTERSECTED.material.color.setHex(this.INTERSECTED.currentHex);
-        }
-        // store reference to closest object as current intersection object
-        this.INTERSECTED = intersects[0].object;
-        // store color of closest object (for later restoration)
-        this.INTERSECTED.currentHex = this.INTERSECTED.material.color.getHex();
-        // set a new color for closest object
-        this.INTERSECTED.material.color.setHex(0x080708);
-      }
-    } else {
-      // restore previous intersection object (if it exists) to its original color
-      if (this.INTERSECTED) {
-        this.INTERSECTED.material.color.setHex(this.INTERSECTED.currentHex);
-      }
-      // remove previous intersection object reference
-      //     by setting current intersection object to "nothing"
-      this.INTERSECTED = null;
-    }
   }
 
   onDocumentMouseClick(event) {
@@ -120,10 +82,9 @@ export class TruckSetUpComponent implements OnInit {
     sphereMesh[2] = inMass2;
 
     const palletHeight = this.chosenData.pallet.height;
-    // const palletCount = this.chosenData.truck.pallets;
 
-    let rotatedNodeId = this.viewer.getSelection()[0];
-    let rotatedBody = { nodeId: rotatedNodeId, fragId: null, fragProxy: null, worldMatrix: null, position: null };
+    const rotatedNodeId = this.viewer.getSelection()[0];
+    const rotatedBody = {nodeId: rotatedNodeId, fragId: null, fragProxy: null, worldMatrix: null, position: null};
     rotatedBody.fragId = this.viewer.impl.model.getData().fragments.fragId2dbId.indexOf(rotatedNodeId);
 
     rotatedBody.fragProxy = this.viewer.impl.getFragmentProxy(this.viewer.impl.model, rotatedBody.fragId);
@@ -137,13 +98,20 @@ export class TruckSetUpComponent implements OnInit {
     rotatedBody.position.copy(rotatedBody.worldMatrix.getPosition().clone());
 
     const geom = new THREE.BoxGeometry(this.chosenData.crate.width, this.chosenData.crate.height, this.chosenData.crate.length);
-    const texture = new THREE.TextureLoader().load( "../../../assets/crate.gif" );
-    const material = new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load( "../../../assets/crate.gif" ) } );
-    console.log(material.map);
-    sphereMesh[0] = new THREE.Mesh(geom, material);
-    sphereMesh[0].position.set(rotatedBody.position.x, rotatedBody.position.y + palletHeight, rotatedBody.position.z);
-    this.viewer.impl.addOverlay('cScene', sphereMesh[0]);
 
-    this.viewer.overlays.impl.invalidate(true);
+    const loader = new THREE.TextureLoader();
+    loader.load(
+      '../../../assets/crate.gif', // src
+      texture => { // onSuccess
+        const material = new THREE.MeshBasicMaterial({map: texture});
+        sphereMesh[0] = new THREE.Mesh(geom, material);
+        sphereMesh[0].position.set(rotatedBody.position.x, rotatedBody.position.y + palletHeight, rotatedBody.position.z);
+        this.viewer.impl.addOverlay('cScene', sphereMesh[0]);
+
+        this.viewer.overlays.impl.invalidate(true);
+      },
+      undefined, // onProcess (not supported anyway)
+      err => console.log(err) // onError
+    );
   }
 }
